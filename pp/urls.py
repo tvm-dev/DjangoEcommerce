f0e2b92ccbbcfd35f2ea9d74d96089ba9bb00349
thiago viana <tvm.dev2015@gmail.com>
2021-09-09 12:30:28 -0300

    starting module users

--- pp/urls.py
+++ pp/urls.py
@@ -7,10 +7,12 @@ from django.views.static import serve as serve_static
 from core import views
 from django.views.static import serve as serve_static
 from catalog import views as views_catalog
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('contato/', views.contact, name='contact'),
+    path('Entrar/', LoginView.as_view(template_name='login.html'), name="login"),
     path('catalogo/', include('catalog.urls', namespace='catalog')),
     path('admin/', admin.site.urls),
 ]
