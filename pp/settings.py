16e3eb0d73c39033ad6f83a48d1f60f42bc8993f
thiago viana <tvm.dev2015@gmail.com>
2021-09-24 16:58:17 -0300

    removed : in the file Procfile

--- pp/settings.py
+++ pp/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qb*baaj2cl_!rlo1()bwb2nd0atnfh(rqncox-yg%ts-g2wo-z'
 
 #SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = false
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
