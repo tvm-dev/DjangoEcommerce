6f802f4819ce32323a07e5cc5c33f30258ba56d9
thiago viana <tvm.dev2015@gmail.com>
2021-09-24 22:40:37 -0300

    userd git revert to fix settings

--- pp/settings.py
+++ pp/settings.py
@@ -2,9 +2,15 @@ from pathlib import Path
 import os
 import django_heroku
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-qb*baaj2cl_!rlo1()bwb2nd0atnfh(rqncox-yg%ts-g2wo-z'
@@ -48,14 +54,7 @@ MIDDLEWARE = [
     'checkout.middleware.cart_item_middleware',
 ]
 
-
 ROOT_URLCONF = 'pp.urls'
-
-# Simplified static file serving.
-# https://warehouse.python.org/project/whitenoise/
-
-
-
 
 TEMPLATES = [
     {
@@ -129,12 +128,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 
-## Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
+#STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
+django_heroku.settings(locals())
+
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
