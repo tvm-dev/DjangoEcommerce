990a3fa363786bdb308623d92b25d9b51684d176
thiago viana <tvm.dev2015@gmail.com>
2021-09-24 23:01:46 -0300

    Try fix settings for static files

--- pp/settings.py
+++ pp/settings.py
@@ -6,10 +6,11 @@ import os
 import django_heroku
 
 
-
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+#BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -131,17 +132,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 
-#STATIC_ROOT = BASE_DIR / 'staticfiles'
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 django_heroku.settings(locals())
-
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 
 
