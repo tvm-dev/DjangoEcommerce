8ba0dd28a74cb07ccc9ada176265a7387636eec4
thiago viana <tvm.dev2015@gmail.com>
2021-09-24 23:22:49 -0300

    try

--- pp/settings.py
+++ pp/settings.py
@@ -14,13 +14,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qb*baaj2cl_!rlo1()bwb2nd0atnfh(rqncox-yg%ts-g2wo-z'
+SECRET_KEY = os.getenv('SECRET_KEY', '123')
+
 
 #SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -133,11 +131,18 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 
+STATIC_ROOT = "/core/assets/"
+
 ALLOWED_HOSTS = ['*']
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/core/assets/',
+]
 
 
 
