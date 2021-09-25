3594332106e5656b84bee174ff7a085317d1e132
thiago viana <tvm.dev2015@gmail.com>
2021-09-24 22:03:55 -0300

    fix problems static files online

--- pp/settings.py
+++ pp/settings.py
@@ -131,14 +131,14 @@ USE_TZ = True
 #STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = "/static/"
+STATIC_URL = '/static/'
 
 django_heroku.settings(locals())
 
 
-#STATICFILES_DIRS = [
- #   os.path.join(BASE_DIR, 'static')
-#]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 
