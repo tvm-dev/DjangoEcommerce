c748fd2f14b33b9fdc527b75087b92d2d3e76634
thiago viana <tvm.dev2015@gmail.com>
2021-09-24 22:24:20 -0300

    fix settings host all

--- pp/settings.py
+++ pp/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = 'django-insecure-qb*baaj2cl_!rlo1()bwb2nd0atnfh(rqncox-yg%ts-g2wo-z
 #SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://git-cursos.herokuapp.com/']
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
