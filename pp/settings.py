9a711d2e5ddee0f32e803b6d4869982c9c4dd237
thiago viana <tvm.dev2015@gmail.com>
2021-09-27 19:32:18 -0300

    restore Settings OK, tests

--- pp/settings.py
+++ pp/settings.py
@@ -131,11 +131,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 
 ALLOWED_HOSTS = ['*']
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
